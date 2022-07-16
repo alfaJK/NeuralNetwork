8be3f2efb9465d7ba1661b145eaa659af2a2619a
Даниил Никоненко <alfa_fk@mail.ru>
2022-07-16 17:09:42 +0300

    change matrix

--- includes/IOData/Matrix.h
+++ includes/IOData/Matrix.h
@@ -1,14 +1,24 @@
 #include "IOData.h"
 #include "vector"
-class Matrix:public IOData{
+
+struct MatrixSize{
+    int height;
+    int width;
+    int depth;
+}
+
+
+class Matrix{
     private:
-        int height;
-        int width;
-        int depth;
+        MatrixSize Size;
+        std::vector<double> MatrixValues;
     public: 
         Matrix(const Matrix& MatrixCopy);
         Matrix(int w , int h , int d);
-        void SplotMatrix(Matrix* Filter);
+        MatrixSize GetSize();
+        double& operator()(int h_i, int w_j, int d_k);
+	    double operator()(int h_i, int w_j, int d_k) const;
+	   // friend std::ostream& operator<<(std::ostream& os, const Tensor& tensor);
     //Matrix operator+(const Matrix* SecondMatrix);
     //Matrix operator*(const float* AnyNum);
 };
--- src/IOData/Matrix.cpp
+++ src/IOData/Matrix.cpp
@@ -1,34 +1,62 @@
 #include "Matrix.h"
 #include "CudaCalc.h"
 
-Matrix::Matrix(const Matrix& MatrixCopy){
-
+Matrix::Matrix(int w, int h, int d) {
+	Size.width = w;
+	Size.height = h;
+	Size.depth = d;
+	Tensor_Values = std::vector<double>(w * h * d, 0);
+}
+//Matrix::~Matrix() { Tensor_Values.clear(); }
+MatrixSize GetSize(){
+    return Size;
 }
 
-Matrix::Matrix(int w , int h , int d): width(w), height(h), depth(d){
-    MatrixData = std::vector<double>(w * h * d, 0);
+Matrix::Matrix(const Matrix& MatrixCopy) {
+	this->Size.width = MatrixCopy.GetSize().width;
+	this->Size.height = MatrixCopy.GetSize().height;
+	this->Size.depth = MatrixCopy.GetSize().depth;
+
+	MatrixValues = std::vector<double>(this->Size.width * this->Size.height * this->Size.depth, 0);
+
+	for (int y = 0; y < this->Size.height; y++) {
+		for (int x = 0; x < this->Size.width; x++) {
+			for (int d = 0; d < this->Size.depth; d++) {
+				this->MatrixValues[y * this->Size.depth * this->Size.width + x * this->Size.depth + d] = MatrixCopy(y,x,d);
+			}
+		}
+	}
 }
 
-void Matrix::SplotMatrix(Matrix* Filter){
-    gridSize = dim3(W / BLOCK_SIZE, W / BLOCK_SIZE, 1);
-    blockSize = dim3(BLOCK_SIZE, BLOCK_SIZE, 1);
-    cudaEventCreate(&Start);
-    cudaEventCreate(&Stop);
-    double* CudaData
-    cudaMallocManaged(&CudaData, width * height * depth * sizeof(int));
-    cudaMemcpy(Data, MatrixData, width * height * depth * sizeof(int), cudaMemcpyHostToDevice);
-    cudaEventRecord(Start);
-
-    
-
-    // NotOptAdditionMatrix <<<gridSize, blockSize >>> (InputMatrix, Filter, OutputMatrix);
-    //AdditionMatrix << <gridSize, blockSize >>> (InputMatrix, Filter, OutputMatrix);
-
-    cudaEventRecord(Stop);
-    cudaDeviceSynchronize();
-    cudaGetLastError();
-    cudaMemcpy(Data2, Data, width * height * sizeof(int), cudaMemcpyDeviceToHost);
+Matrix::Matrix(MatrixSize Size) {
+	this->Size.width = Size.width;
+	this->Size.height = Size.height;
+	this->Size.depth = Size.depth;
+	MatrixValues = std::vector<double>(this->Size.width * this->Size.height * this->Size.depth, 0);
 }
 
-//Matrix operator+(const Matrix* SecondMatrix);
-//Matrix operator*(const float* AnyNum);
+double& Matrix::operator()(int h_i, int w_j, int d_k) {
+	return MatrixValues[h_i * Size.depth * Size.width + w_j * Size.depth + d];
+}
+
+double Matrix::operator()(int h_i, int w_j, int d_k) const {
+	return MatrixValues[h_i * Size.depth * Size.width + w_j * Size.depth + d];
+}
+
+/*
+====================вывод данных===========================
+
+std::ostream& operator<<(std::ostream& os, const Tensor& tensor) {
+	for (int d = 0; d < tensor.size.depth; d++) {
+		for (int i = 0; i < tensor.size.height; i++) {
+			for (int j = 0; j < tensor.size.width; j++)
+				os << tensor.Tensor_Values[i * (tensor.size.depth * tensor.size.width) + j * tensor.size.depth + d] << " ";
+	
+		//	os << std::endl;
+		}
+
+	//	os << std::endl;
+	}
+	os << std::endl;
+	return os;
+}*/
